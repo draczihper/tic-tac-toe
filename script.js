eda973307d063248515d029346d2f33e8e1f5b22
Drac Zihper <draczihper@gmail.com>
2024-05-05 23:00:56 +0300

    Add a result function

--- script.js
+++ script.js
@@ -8,8 +8,15 @@ if (playerOne == "O") {
 }
 console.log(`Player one is ${playerOne} and player two is ${playerTwo}`);
 
+function result(player) {
+  console.log(`GAME OVER!! ${player} won`)
+}
+
+if ((board[0], board[1], board[2]) || (board[3], board[4], board[5]) || (board[6], board[7], board[8]) || (board[0], board[3], board[6]) || (board[1], board[4], board[7]) || (board[2], board[5], board[8]) || (board[0], board[4], board[8]) || (board[2], board[4], board[6]) === playerOne || playerTwo ) {
+  result(playerOne || playerTwo);
+}
+
 let board = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// let boardCopy = [...board];
 function drawBoard() {
   console.log(`
         --BOARD--
@@ -42,4 +49,7 @@ function playerTwoMoveFunc() {
   }
 }
   playerOneMoveFunc();
+
+
+
 
